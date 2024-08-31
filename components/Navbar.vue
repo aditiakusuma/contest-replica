acb72d8cd0841c7c8a8ee45db6107922c777d0e5
aditiakusuma <aditiakusuma800@gmail.com>
2024-08-31 15:14:48 +0700

    init nav

---
+++ components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <nav
+    class="fixed top-0 inset-x-0 h-14 bg-white flex justify-between items-center"
+  >
+    <div class="w-[70px] flex justify-center h-12 items-center">
+      <button
+        class="border-primary border text-primary flex justify-center items-center rounded-xl w-[42px] h-[42px]"
+      >
+        <Icon name="heroicons:bars-3-bottom-left-20-solid" size="24" />
+      </button>
+    </div>
+    <div class="w-[70px] flex-1 flex justify-center h-12 items-center">
+        <img src="https://system.dilanatandoor.com.my/theme/assets/images/mylogo.png" height="45" width="173" alt="">
+    </div>
+    <div class="w-[70px] flex justify-center h-12 items-center">
+      <button
+        class="border-primary border text-primary flex justify-center items-center rounded-xl w-[42px] h-[42px]"
+      >
+        <Icon name="ri:shopping-cart-line" size="24" />
+      </button>
+    </div>
+  </nav>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        Cek
+    </div>
+</template>
