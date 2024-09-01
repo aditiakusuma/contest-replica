e81b789d2524d46499c29900101637d83c838cec
aditiakusuma <aditiakusuma800@gmail.com>
2024-09-02 02:36:14 +0700

    add credit page

---
+++ components/Credit/NoHistory.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex flex-col items-center">
+    <img
+      src="~/assets/images/nohistory.png"
+      class="w-3/4 aspect-3/4 max-w-2xl mb-4"
+      alt=""
+    />
+    <div class="text-lg mb-1">No History Found</div>
+    <p class="text-sm">Please try again later!</p>
+  </div>
+</template>
---
+++ pages/credits.vue
@@ -0,0 +1,26 @@
+<template>
+    <NuxtLayout name="page" title="Profile" @back="() => console.log('back')">
+      <div class="px-4 py-20 w-full">
+        <div class="flex flex-col gap-y-4 max-w-lg mx-auto md:border md:p-8">
+          <div class="font-bold text-2xl" >My Credit</div>
+          <div class="shadow-lg p-4">
+            <p class="text-xs" >Available Balance</p>
+            <div class="flex justify-between">
+                <div class="text-2xl" >0.0</div>
+                <button class="rounded text-xs bg-green-400 p-1 h-min">Add Balance</button>
+            </div>
+          </div>
+          <div>Transaction History</div>
+          <div class="border-b flex justify-between p-4">
+                <p>Filter Date</p>
+                <Icon name="lucide:calendar-days" class="text-primary" size="24" />
+          </div>
+          <CreditNoHistory />
+        </div>
+      </div>
+    </NuxtLayout>
+  </template>
+  
+  <script setup lang="ts">
+  </script>
+  
