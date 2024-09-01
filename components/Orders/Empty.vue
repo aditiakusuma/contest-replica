d70564e75b961f8b5c75d9db775164407f93a01c
aditiakusuma <aditiakusuma800@gmail.com>
2024-09-02 01:35:05 +0700

    add empty order

---
+++ components/Orders/Empty.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex flex-col items-center">
+    <img
+      src="~/assets/images/no-record.png"
+      class="w-3/4 aspect-3/4 max-w-2xl mb-4"
+      alt=""
+    />
+    <div class="text-lg mb-1">NO ACTIVE ORDERS FOUND</div>
+  </div>
+</template>
---
+++ pages/orders.vue
@@ -0,0 +1,22 @@
+<template>
+  <NuxtLayout name="page" title="My Orders" @back="() => console.log('back')">
+    <div class="px-4 py-20 w-full">
+      <div class="flex flex-col gap-y-4 max-w-lg mx-auto md:border">
+        <div class="flex font-bold text-sm text-center">
+          <div class="relative flex-1">
+            <button class="h-12 cursor-pointer text-primary relative">
+              ACTIVE ORDERS (0)
+            </button>
+            <div class="absolute h-1 bottom-0 inset-x-0 bg-primary"></div>
+          </div>
+          <div class="relative flex-1">
+            <button class="h-12 cursor-pointer relative">
+              PAST ORDERS (0)
+            </button>
+          </div>
+        </div>
+        <OrdersEmpty />
+      </div>
+    </div>
+  </NuxtLayout>
+</template>
