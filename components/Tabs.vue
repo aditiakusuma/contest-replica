679dbd12cf128c8c914d58b383dc0392e92a1fb6
aditiakusuma <aditiakusuma800@gmail.com>
2024-09-01 13:47:51 +0700

    add tabs

---
+++ components/Tabs.vue
@@ -0,0 +1,30 @@
+<template>
+  <div
+    class="fixed z-10 bg-white bottom-0 inset-x-0 h-14 flex justify-center shadow-2xl"
+  >
+    <NuxtLink
+      v-for="({ name, iconName, link }, index) in tabs"
+      :key="index"
+      :to="link"
+      class="h-full max-w-40 w-1/5 flex flex-col justify-center items-center"
+      :class="$route.path === link ? 'text-primary' : 'text-gray-400'"
+    >
+      <Icon :name="iconName" size="22" />
+      <div class="text-xs font-medium">{{ name }}</div>
+    </NuxtLink>
+  </div>
+</template>
+
+<script setup lang="ts">
+const tabs = [
+  { iconName: "solar:home-angle-2-outline", name: "home", link: "/" },
+  {
+    iconName: "material-symbols:credit-score-outline-rounded",
+    name: "credits",
+    link: "/credits",
+  },
+  { iconName: "ri:shopping-cart-line", name: "cart", link: "/cart" },
+  { iconName: "solar:checklist-linear", name: "orders", link: "/orders" },
+  { iconName: "ion:person-outline", name: "Profile", link: "/profile" },
+];
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,5 +2,6 @@
   <div>
     <Navbar class="z-10" />
     <slot />
+    <Tabs />
   </div>
 </template>
