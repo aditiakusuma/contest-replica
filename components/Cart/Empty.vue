19dfb3c14edf2a8ec2dc70832da6ba75883b45f8
aditiakusuma <aditiakusuma800@gmail.com>
2024-09-01 20:46:40 +0700

    add empty cart

---
+++ components/Cart/Empty.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="flex flex-col items-center">
+        <img src="~/assets/images/no-cart.png" class="w-3/4 aspect-3/4 max-w-2xl mb-4" alt="">
+        <div class="text-lg mb-1">NO ITEMS IN CART</div>
+        <p class="text-sm">Please add items to cart!</p>
+    </div>
+</template>
---
+++ pages/cart.vue
@@ -0,0 +1,10 @@
+<template>
+  <NuxtLayout name="page" title="Cart" @back="() => console.log('back')">
+    <template #icon-right>
+            <Icon name="hugeicons:delete-02" size="32" class="cursor-pointer" />
+    </template>
+    <div class="px-4 py-20 w-full">
+      <CartEmpty />
+    </div>
+  </NuxtLayout>
+</template>
